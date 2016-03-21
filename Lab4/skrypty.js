cd71305c33c633cadc03879b8231543fe43f7e0f
Mateusz Boroń <mboron93@wp.pl>
2016-03-22 00:43:53 +0100

    lab4

---
+++ Lab4/skrypty.js
@@ -0,0 +1,44 @@
+//chowanie tekstu
+$(document).ready(function(){
+    $("#1").click(function(){
+        $("#hide").toggle();
+    });
+});
+
+//panelek
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});
+
+//animacja kwadracika
+$(document).ready(function(){
+$("#2").click(function(){
+    var div = $("#kwadrat");
+    div.animate({left: '600px',height: '300px', opacity: '0.4'}, "slow");
+    div.animate({width: '300px', opacity: '0.8'}, "slow");
+    div.animate({height: '100px', opacity: '0.4'}, "slow");
+    div.animate({width: '100px', opacity: '0.8'}, "slow");
+	});
+});  
+
+//ukrywanie tekstu
+$(document).ready(function(){
+    $("#Ukrywanie").click(function(){
+        $("#Ukryw").hide("slow", function(){
+            alert("Ukryty mozesz byc spokojny");
+        });
+    });
+});
+
+//Usuwanie tekstu
+$(document).ready(function(){
+    $("#usun").click(function(){
+        $("#div1").empty();
+		alert("Jak mogles usunac tekst :<");
+    });
+});
+
+
+
